c155d1f097b35315dc88a28e8b248a3544851193
YarinQuapi <41652571+YarinQuapi@users.noreply.github.com>
2022-05-20 23:27:06 +0300

    Create AdminCommand.java

---
+++ src/main/java/me/yarinlevi/waypoints/commands/administration/AdminCommand.java
@@ -0,0 +1,42 @@
+package me.yarinlevi.waypoints.commands.administration;
+
+import me.yarinlevi.waypoints.commands.SubCommand;
+import me.yarinlevi.waypoints.commands.administration.sub.CheckSubCommand;
+import me.yarinlevi.waypoints.commands.administration.sub.ChunkScanSubCommand;
+import me.yarinlevi.waypoints.gui.GuiUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author YarinQuapi
+ **/
+public class AdminCommand implements CommandExecutor {
+    private final Map<String, SubCommand> commandMap = new HashMap<>();
+
+    public AdminCommand() {
+        commandMap.put("check", new CheckSubCommand());
+        commandMap.put("chunkscan", new ChunkScanSubCommand());
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            if (commandMap.containsKey(args[0].toLowerCase())) {
+                commandMap.get(args[0].toLowerCase()).run(player, args);
+                return true;
+            } else {
+                sender.sendMessage("Sorry, couldn't find the command.");
+                return false;
+            }
+        }
+
+        sender.sendMessage("You are required to be a player to execute this command.");
+        return false;
+    }
+}
